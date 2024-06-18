c238a9cfc14e44a6e7a934abdd0cd59c9db23801
linetlucy-genchabe <linetlucy21@gmail.com>
2024-06-18 19:28:13 +0300

    data update forms

--- Repoapp/forms.py
+++ Repoapp/forms.py
@@ -11,6 +11,14 @@ class NewAccountForm(forms.ModelForm):
           'account': forms.Textarea(attrs={'rows':2, 'cols':10,}),
         }
 
+class UpdateAccountForm(forms.ModelForm):
+    class Meta:
+        model = Accounts
+        exclude = ['Author', 'pub_date', 'author_profile','admin_profile', ]
+        widgets = {
+          'account': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+        }
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
