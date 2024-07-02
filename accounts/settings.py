4ffee9281cda9a51d2758bb7bcf7b80612acccd1
linetlucy-genchabe <linetlucy21@gmail.com>
2024-07-02 21:32:20 +0300

    update settings

--- accounts/settings.py
+++ accounts/settings.py
@@ -63,7 +63,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'repoapp', 'templates')
+            ['./Repoapp/templates/']
         ],
         'APP_DIRS': True,
         'OPTIONS': {
