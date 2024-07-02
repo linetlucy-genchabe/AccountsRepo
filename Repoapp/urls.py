ae5674a465585c23c1117edebdcdf72e4f2075f7
linetlucy-genchabe <linetlucy21@gmail.com>
2024-07-02 12:56:12 +0300

    template urls

--- Repoapp/urls.py
+++ Repoapp/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     url(r'login/$',views.user_login, name='login'),
     url(r'logout/$',views.signout),
     url(r'^new/account$', views.new_account, name='new-account'),
-    
+    url(r'^kisumu$', views.kisumu, name= 'kisumu'),
+    url(r'^busia$', views.busia, name= 'busia'),
     url(r'^search/', views.search_accounts, name='search_results'),
     url(r'^profile/$', views.user_profiles, name='profile'),
     path('update-account/<int:id>', views.update_account, name="update-account"),
