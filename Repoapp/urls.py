e6d78092543198f1ac324e87e277c2f4765e2ef0
linetlucy-genchabe <linetlucy21@gmail.com>
2024-07-02 16:18:26 +0300

    procfile

--- Repoapp/urls.py
+++ Repoapp/urls.py
@@ -5,12 +5,12 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$', views.index, name= 'index'),
+    url(r'^$', views.index, name='index'),
     url(r'login/$',views.user_login, name='login'),
     url(r'logout/$',views.signout),
     url(r'^new/account$', views.new_account, name='new-account'),
-    url(r'^kisumu$', views.kisumu, name= 'kisumu'),
-    url(r'^busia$', views.busia, name= 'busia'),
+    url(r'^kisumu$', views.kisumu, name='kisumu'),
+    url(r'^busia$', views.busia, name='busia'),
     url(r'^search/', views.search_accounts, name='search_results'),
     url(r'^profile/$', views.user_profiles, name='profile'),
     path('update-account/<int:id>', views.update_account, name="update-account"),
