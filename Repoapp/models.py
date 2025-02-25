6ba8580d807e1c98bb2a7f5238030323cf74fa28
linetlucy-genchabe <linetlucy21@gmail.com>
2025-02-25 10:15:43 +0300

    lms accounts model

--- Repoapp/models.py
+++ Repoapp/models.py
@@ -204,3 +204,74 @@ class Dashboards(models.Model):
         ordering = ['-pub_date']
         verbose_name = 'My Dashboards'
         verbose_name_plural = 'Dashboards'
+
+        
+class Lmsaccounts(models.Model):
+    Name = models.CharField(max_length=255)
+    Community_Health_Unit = models.CharField(max_length=255)
+    Username = models.CharField(max_length=255)
+    Password = models.CharField(max_length=255)
+    account_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    account_subcounty = models.ForeignKey(Subcounty, on_delete=models.CASCADE,related_name="lmsaccountnames")
+    account_county = models.ForeignKey(County, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    admin_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    
+    
+    def save_lmsaccounts(self):
+        self.save()
+    
+    def delete_lmsaccounts(self):
+        self.delete()
+        
+    @classmethod
+    def get_alllmsaccounts(cls):
+        lms = cls.objects.all()
+        return lms
+    
+    @classmethod
+    def search_lmsaccounts(cls, search_term):
+        lmsaccounts = cls.objects.filter(Username__icontains=search_term)| cls.objects.filter(Name__icontains=search_term)
+
+        
+        return lmsaccounts
+    
+    @classmethod
+    def get_by_Category(cls, categories):
+        lmsaccounts = cls.objects.filter(category__name__icontains=categories)
+        return lmsaccounts
+    
+    @classmethod
+    def get_by_County(cls, counties):
+        lmsaccounts = cls.objects.filter(category__name__icontains=counties)
+        return lmsaccounts
+    @classmethod
+    def get_by_Subcounty(cls, subcounty):
+        return cls.objects.filter(account_subcounty=subcounty)
+
+    # @classmethod
+    # def get_by_Subcounty(cls, subcounties):
+    #     accounts = cls.objects.filter(category__name__icontains=subcounties)
+    #     return accounts
+    
+    @classmethod
+    def get_accounts(request, id):
+        try:
+            lmsaccount = Lmsaccounts.objects.get(pk = id)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return lmsaccount
+    
+    def update_lmsaccounts(self):
+        self.update_lmsaccounts()
+    
+    def __str__(self):
+        return self.Name
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My lmsaccounts'
+        verbose_name_plural = 'lmsaccounts'
