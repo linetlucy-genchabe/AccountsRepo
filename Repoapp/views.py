08d720a450692ae1987ecd7b463c452699f051c1
Linet Lucy Genchabe <lgenchabe@livinggoods.org>
2026-03-11 20:04:39 +0300

    migrations issue fix

--- Repoapp/views.py
+++ Repoapp/views.py
@@ -17,6 +17,14 @@ from django.db.models import Q
 
 FULL_ACCESS_ROLES = ['Admin', 'Superuser', 'MOH', 'RDHSO', 'UserManager']
 
+
+from django.core.management import call_command
+from django.http import HttpResponse
+
+def run_migrations(request):
+    call_command('migrate')
+    return HttpResponse("Migrations complete")
+
 @login_required(login_url='/login/')
 def index(request):
     profile = request.user.profile
