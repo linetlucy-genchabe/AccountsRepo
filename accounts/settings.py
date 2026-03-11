d442a2b7e57c55386d169065dc95ad148ec12d1d
Linet Lucy Genchabe <lgenchabe@livinggoods.org>
2026-03-12 00:01:45 +0300

    fix: set conn_max_age=0 and conn_health_checks=True

--- accounts/settings.py
+++ accounts/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 DATABASE_URL= 'postgresql://postgres:srXrmMJMCEytsZmoHYoFrmHMgmDtKFpe@monorail.proxy.rlwy.net:13964/railway'
 DATABASES = {
     'default': 
-    dj_database_url.parse(DATABASE_URL,conn_max_age=600),
+    dj_database_url.parse(DATABASE_URL, conn_max_age=0, conn_health_checks=True),
 }
 
 
