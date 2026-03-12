3201d3c3bed92895151d31a53fc4c83ba79c08b0
Linet Lucy Genchabe <lgenchabe@livinggoods.org>
2026-03-12 08:47:40 +0300

    fix: load signals in AppConfig.ready() to auto-create user profiles

--- Repoapp/apps.py
+++ Repoapp/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class RepoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Repoapp'
+
+    def ready(self):
+        import Repoapp.signals
